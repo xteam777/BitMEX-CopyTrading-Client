c640c0c707adfd2ba851eebc999aed78d4d14344
xteam777 <38719485+xteam777@users.noreply.github.com>
2022-02-15 11:44:20 +0300

    Add files via upload

---
+++ MCClient.dpr
@@ -0,0 +1,33 @@
+program MCClient;
+
+{$R *.dres}
+
+uses
+  {$IFDEF EurekaLog}
+  EMemLeaks,
+  EResLeaks,
+  EDialogWinAPIMSClassic,
+  EDialogWinAPIEurekaLogDetailed,
+  EDialogWinAPIStepsToReproduce,
+  EDebugExports,
+  EDebugJCL,
+  EFixSafeCallException,
+  EMapWin32,
+  EAppVCL,
+  ExceptionLog7,
+  {$ENDIF EurekaLog}
+  Forms,
+  uClient in 'uClient.pas' {MainForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
+
+
+
+
